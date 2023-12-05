92f8e564403e3e3df5137eddd3fa2dc5b03ce64c
derekvawdrey <derek.vawdrey@gmail.com>
2023-12-05 14:32:38 -0700

    Fixed jobs

--- src/App.js
+++ src/App.js
@@ -9,10 +9,14 @@ function App() {
   return (
     <div className="App">
       <JobContainer>
+        <h1>Current Jobs</h1>
         <RunningJob job_name={"Job #1"} start_time={new Date()}></RunningJob>
         <RunningJob job_name={"MAS student update"} start_time={new Date()}></RunningJob>
+        <hr></hr>
         <Job status={"warning"} job_name={"Job naasdasd asd asd sad asds asdasd ame here"} description={"This is a test so that it wil work"}></Job>
-        <Job job_name={"Job name here"} description={"This is a test so that it wil work"}></Job>
+        <hr></hr>
+        <Job status={"error"} job_name={"Job naasdasd asd asd sad asds asdasd ame here"} description={"This is a test so that it wil work"}></Job>
+        <Job status={"error"} job_name={"Job naasdasd asd asd sad asds asdasd ame here"} description={"This is a test so that it wil work"}></Job>
       </JobContainer>
     </div>
   );
