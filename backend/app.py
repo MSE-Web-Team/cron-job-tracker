f2f92eacea5269ad554d2638303c57afd6483a87
derekvawdrey <derek.vawdrey@gmail.com>
2023-12-07 12:21:23 -0700

    Fixing the api endpoint

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api')
 def api_endpoint():
     return 'Hello from the API endpoint!'
 
-@app.route('/api/')
-def api_endpoint():
-    return '/api/ endpoint'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
