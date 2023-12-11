c43d2bb47d40fefe40e98aa7ad2fef502a26640a
derekvawdrey <derek.vawdrey@gmail.com>
2023-12-11 13:40:13 -0700

    Trying Axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,16 +10,7 @@ function App() {
 
   return (
     <div className="App">
-      <JobContainer>
-        <h2>Current Jobs</h2>
-        <RunningJob job_name={"Job #1"} start_time={new Date()}></RunningJob>
-        <RunningJob job_name={"MAS student update"} start_time={new Date()}></RunningJob>
-        <hr></hr>
-        <Job status={"warning"} job_name={"Job naasdasd asd asd sad asds asdasd ame here"} description={"This is a test so that it wil work"}></Job>
-        <hr></hr>
-        <Job status={"error"} job_name={"Job naasdasd asd asd sad asds asdasd ame here"} description={"This is a test so that it wil work"}></Job>
-        <Job status={"error"} job_name={"Job naasdasd asd asd sad asds asdasd ame here"} description={"This is a test so that it wil work"}></Job>
-      </JobContainer>
+      <JobContainer></JobContainer>
       <LogContainer>
         <h2>Current Logs</h2>
         <LogMessage status="error" name="EHIAHDIW" description={"asdasd"}></LogMessage>
--- frontend/src/components/Jobs/JobContainer.js
+++ frontend/src/components/Jobs/JobContainer.js
@@ -1,10 +1,38 @@
-import React, { useState } from 'react';
-import styles from '../commonStyles.module.css';
+import React, { useState, useEffect } from 'react';
 
-const JobContainer = ({ children }) => (
-    <div className={styles.jobFlex}>
-        {children}
+const JobContainer = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await fetch('/api/jobs');
+        const data = await response.json();
+        setJobs(data.jobs);
+      } catch (error) {
+        console.error('Error fetching jobs:', error);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
+  return (
+    <div>
+      <h2>Current Jobs</h2>
+      {jobs.map(job => (
+        <RunningJob
+          key={job.id}
+          job_name={job.process_name}
+          start_time={new Date(job.start_time)}
+        />
+      ))}
+      <hr></hr>
+      {/* Render other job components as needed */}
     </div>
-);
+  );
+};
+
+// Similar component for LogContainer
 
 export default JobContainer;
